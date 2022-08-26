a7cb9d2241e70c36a429e0c1ad6bc2d74a65074a
leadattic <90087384+leadattic@users.noreply.github.com>
2022-08-26 17:18:37 +0000

    Click to increase updates

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 let countEl = document.getElementById("count-el")
 let count = 0
-
+let SaveEl = document.getElementById('save-el')
 function increment() {
     count = count + 1
     countEl.innerText = count
 }
 
 
-function save(){
+
+
+let saved1 = ' '
+let saved2 = ' '
+let saved3 = ' '
+function addtosaved(){
+saved3 = saved2
+saved2 = saved1
+saved1 = count   
+  SaveEl.innerText = 'latest resets at: ' + saved1 + '-' + saved2 + '-' + saved3   
+}
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+    addtosaved()
+    count = 0
+}
+function reset(){
     console.log('you got ' + count)
     alert('you got ' + count)
-    count = 0
+    save()
   countEl.innerText = count
 }
+  SaveEl.innerText = 'latest resets at: ' + saved1 + '-' + saved2 + '-' + saved3  
